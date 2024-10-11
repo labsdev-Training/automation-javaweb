6af2433be8d5c7a43432cc4e7d6973350e9abde0
Rose Dias <rosedias.info@gmail.com>
2024-10-11 15:33:05 -0300

    reuso de código

--- src/test/java/web/Cadastro.java
+++ src/test/java/web/Cadastro.java
@@ -25,6 +25,27 @@ class PontoDoacao{
 
 class Cadastro {
 
+	private void submeterFormulario(PontoDoacao ponto){
+				open("https://petlov.vercel.app/signup");
+				$("h1").shouldHave(text("Cadastro de ponto de doação"));
+				
+				$("input[placeholder='Nome do ponto de doação']").setValue(ponto.nome);
+				$("input[name=email]").setValue(ponto.email);
+				$("input[name=cep]").setValue(ponto.cep);
+				$("input[value='Buscar CEP']").click();
+				$("input[name=addressNumber]").setValue(ponto.numero.toString());
+				$("input[name=addressDetails]").setValue(ponto.complemento);
+				$(By.xpath("//span[text()=\"" + ponto.pets +"\"]/..")).click();
+				$(".button-register").click();
+	}
+
+
+	private void acessarFormulario(){
+
+		open("https://petlov.vercel.app/signup");
+		$("h1").shouldHave(text("Cadastro de ponto de doação"));
+	}
+
 	@Test
 	@DisplayName("Deve poder cadastrar um ponto de doação")
 	void createPoint() {
@@ -37,21 +58,36 @@ class Cadastro {
 			"Prédio Azul",
 			"Cachorros"
 		);
+
+		acessarFormulario();			
 		//Ação
-		open("https://petlov.vercel.app/signup");
-		$("h1").shouldHave(text("Cadastro de ponto de doação"));
-		
-		$("input[placeholder='Nome do ponto de doação']").setValue(ponto.nome);
-		$("input[name=email]").setValue(ponto.email);
-		$("input[name=cep]").setValue(ponto.cep);
-		$("input[value='Buscar CEP']").click();
-		$("input[name=addressNumber]").setValue(ponto.numero.toString());
-		$("input[name=addressDetails]").setValue(ponto.complemento);
-		$(By.xpath("//span[text()=\"" + ponto.pets +"\"]/..")).click();
-		$(".button-register").click();
-		//Resultado
+		submeterFormulario(ponto);
+
+		//Resultado esperado
 		String target = "Seu ponto de doação foi adicionado com sucesso. Juntos, podemos criar um mundo onde todos os animais recebam o amor e cuidado que merecem.";
 		$("#success-page p").shouldHave(text(target));
+	}
+
+	@Test
+	@DisplayName("Não deve cadastrar com email inválido")
+	void emailIncorreto() {
+		//Pré-condição
+		PontoDoacao ponto = new PontoDoacao(
+			"Lar dos Cats",
+			"atendimento&point.com.br",
+			"04534011",
+		   1000,
+			"Prédio Azul",
+			"Gatos"
+		);
+		acessarFormulario();		
+
+		//Ação
+		submeterFormulario(ponto);
+
+		//Resultado esperado
+		String target = "Informe um email válido";
+		$(".alert-error").shouldHave(text(target));
 	}
 }
 
